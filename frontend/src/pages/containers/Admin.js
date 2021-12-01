1ad7e7074d28e7f69bdd38d33e1aaa2e700e7eec
violazhao <violaxzhao@gmail.com>
2021-12-01 14:39:51 -0600

    working on linking recommended games to frontend

--- frontend/src/pages/containers/Admin.js
+++ frontend/src/pages/containers/Admin.js
@@ -1,11 +1,12 @@
 import React, { useState,useEffect } from "react";
 import "./Admin.css";
 import { getFavorite } from "../../utils/apiWrapper";
+import { getRecommendedGames } from "../../utils/apiWrapper";
 import Results from "./Results";
 
 export default function Admin() {
   const [games, setGames] = useState([]);
-  
+  const [recGames, setRecGames] = useState([]);
   
   useEffect(() => {
     const getResults = async () => {
@@ -20,12 +21,25 @@ export default function Admin() {
     getResults()
   }, [])
 
+  useEffect(() => {
+    const getResults = async () => {
+
+      const resp = await getRecommendedGames();
+      if (!resp.error) {
+        console.log(resp.data);
+        setRecGames(resp.data);
+      }
+    }
+
+    getResults()
+  }, [])
 
   return (
     <div className="admin">
       <header className="Admin-header">
         <h1 style={{ marginTop: 16 }}>Your Favorite Games</h1>
         <p>You may also like: </p>
+        
         <Results games={games} isAdmin={true}/>
         <br></br>
       </header>
